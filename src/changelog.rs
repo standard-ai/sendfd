97748d112224a2727adc2b16c8868a5b487caaba
Simonas Kazlauskas <git@kazlauskas.me>
2019-04-11 22:34:59 +0300

    rustfmt changelog

--- src/changelog.rs
+++ src/changelog.rs
@@ -1,6 +1,5 @@
 //! Project changelog
 
-
 /// Release 0.3.0
 ///
 /// * Removed the `Receivable` trait, because it is difficult to write meaningful code with `<T as
@@ -8,19 +7,16 @@
 /// * Removed the `Sendable` trait. While the generalisation here is sound and is beneficial, the
 /// inconsistency between ability to send any sendable and then only being able to receive a
 /// `RawFd` was deemed to be not worth it.
-pub mod r0_3_0 {
-}
+pub mod r0_3_0 {}
 
 /// Release 0.2.1
 ///
 /// Removed an accidentally publicly exported internal function.
 ///
 /// 0.2.0 has been yanked.
-pub mod r0_2_1 {
-}
+pub mod r0_2_1 {}
 
 /// Release 0.2.0
 ///
 /// Pure-Rust reimplementation of the crate.
-pub mod r0_2_0 {
-}
+pub mod r0_2_0 {}
